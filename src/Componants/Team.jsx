cd86d26e9e40a5eeccb1bc73f092ab1240de6d30
Debashis Roy <goodquote39@hotmail.com>
2023-05-30 16:27:03 +0600

    bacground changed

--- src/Componants/Team.jsx
+++ src/Componants/Team.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Team = ({ team }) => {
-    const { strTeam, strTeamShort, intFormedYear, strCountry, strTeamLogo } = team;
-    // console.log(strTeam)
-    return (
-        <div className='rounded-lg p-2 text-center shadow-inner shadow-black hover:shadow-xl hover:bg-sky-200 hover:scale-105 transition-all duration-700'>
-            <img src={strTeamLogo} alt={strTeam} />
-            <h2 className='text-2xl font-semibold italic'>{strTeam}</h2>
-            <h3>Short name : {strTeamShort}</h3>
-            <h4>Team formed in : {intFormedYear}</h4>
-            <Link to={`/${strCountry}`}>
-                <button className='bg-black text-white py-1 px-3 w-full rounded-full hover:scale-90 transition-all duration-500 '>{strCountry}</button>
-            </Link>
-            
-        </div>
-    );
+  const { strTeam, strTeamShort, intFormedYear, strCountry, strTeamLogo } =
+    team;
+  // console.log(strTeam)
+  return (
+    <div className="rounded-lg p-2 text-center bg-blue-100 shadow-inner shadow-black hover:shadow-xl hover:bg-sky-200 hover:scale-105 transition-all duration-700">
+      <img
+        src={strTeamLogo}
+        alt={strTeam}
+      />
+      <h2 className="text-2xl font-semibold italic">{strTeam}</h2>
+      <h3>Short name : {strTeamShort}</h3>
+      <h4>Team formed in : {intFormedYear}</h4>
+      <Link to={`/${strCountry}`}>
+        <button className="bg-black text-white py-1 px-3 w-full rounded-full hover:scale-90 hover:bg-blue-950 transition-all duration-500 ">
+          {strCountry}
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default Team;
--- src/Componants/Teams.jsx
+++ src/Componants/Teams.jsx
@@ -1,46 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Team from './Team';
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Team from "./Team";
 import PulseLoader from "react-spinners/PulseLoader";
 
 const Teams = () => {
-    const [loading, setLoading] = useState(true)
-    const data = useLoaderData();
-    const teams = data?.teams;
+  const [loading, setLoading] = useState(true);
+  const data = useLoaderData();
+  const teams = data?.teams;
 
-     useEffect(() => {
+  useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 2000);
   }, []);
-    // console.log(teams);
-    return (
-        <div className='text-center'>
-            <h4 className='text-center text-3xl font-semibold'>{ teams?.length} teams in this league</h4>
-            {
-                loading?
-                <PulseLoader
-                        color="#36d7b7"
-                        loading={loading}
-                        cssOverride=""
-                        size={50}
-                        aria-label="Loading Spinner"
-                        data-testid="loader"
-                    />
-                    :
-                        <div className=' grid grid-cols-4 p-3 my-10 gap-4'>
-                            {
-                                teams?.map(team => (
-                                    <Team
-                                        key={team?.idTeam}
-                                        team={team}
-                                    ></Team>
-                                ))
-                            }
-                        </div>
-            }
+  // console.log(teams);
+  return (
+    <div className="text-center">
+      <h4 className="text-center text-3xl font-semibold text-sky-100">
+        {teams?.length} teams in this league
+      </h4>
+      {loading ? (
+        <PulseLoader
+          color="#36d7b7"
+          loading={loading}
+          cssOverride=""
+          size={50}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <div className=" grid grid-cols-4 p-3 my-10 gap-4">
+          {teams?.map((team) => (
+            <Team
+              key={team?.idTeam}
+              team={team}
+            ></Team>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Teams;
